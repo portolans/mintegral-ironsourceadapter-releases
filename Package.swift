6f4eb97ce5eca1d03105396ca93fa65dd9b9f0fa
Portolabot <portolabot@hiportola.com>
2026-01-21 01:36:43 +0000

    Update to 5.5.0

--- Package.swift
+++ Package.swift
@@ -1,1 +1,22 @@
+// swift-tools-version:6.2
+import PackageDescription
 
+let package = Package(
+    name: "ISMintegralAdapter",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "ISMintegralAdapter",
+            targets: ["ISMintegralAdapter"],
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ISMintegralAdapter",
+            url: "https://github.com/portolans/mintegral-ironsourceadapter-releases/releases/download/5.5.0/ISMintegralAdapter.xcframework.zip",
+            checksum: "673247a1544c3d9d92f5bdc19efd6a50dc80f1592fdfe24c6d47f0f29970dea7",
+        ),
+    ],
+)
