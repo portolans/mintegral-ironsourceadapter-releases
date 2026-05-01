af3d17d7b3790f3d77b617218a442d71d4c7dfaa
Portolabot <portolabot@hiportola.com>
2026-05-01 07:06:00 +0000

    Update to 5.12.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ISMintegralAdapter",
-            url: "https://github.com/portolans/mintegral-ironsourceadapter-releases/releases/download/5.11.0/ISMintegralAdapter.xcframework.zip",
-            checksum: "5337b08ae3a7e1022ff9100855df6a55833194ae4f80412e71da9370c1314cce",
+            url: "https://github.com/portolans/mintegral-ironsourceadapter-releases/releases/download/5.12.0/ISMintegralAdapter.xcframework.zip",
+            checksum: "97b47cd73961c4c2039bda191c916f5c9bc113b8bc4b3c076e0f2f7307d9c1b0",
         ),
     ],
 )
